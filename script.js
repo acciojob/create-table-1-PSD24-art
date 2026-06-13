eb0e888d4da3ac5ca52a134f5f837c24bae16e28
Prathamesh S. Dahake <pratham.dahakepsd@gmail.com>
2026-06-13 15:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const sampleTable = document.getElementById("sampleTable")
+console.dir(sampleTable);
+
 function insert_Row() {
     //Write your code here
-  
-  
+	  sampleTable.innerHTML = `<tr>
+	  <td>New Cell1</td>
+	  <td>New Cell2</td>
+	  </tr>` + sampleTable.innerHTML;
 }
